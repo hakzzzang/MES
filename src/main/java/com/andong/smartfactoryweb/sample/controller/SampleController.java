a7326dca953cab07893c72e01c13ac0530c7a520
hakzzzang <it.develop.soo@gmail.com>
2023-11-18 16:27:42 +0900

    Test

--- src/main/java/com/andong/smartfactoryweb/sample/controller/SampleController.java
+++ src/main/java/com/andong/smartfactoryweb/sample/controller/SampleController.java
@@ -4,19 +4,21 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/sample")
 @Slf4j
 public class SampleController {
     @GetMapping("/getMapping")
-    public String SampelGetMappingTest(){
+    public String SampelGetMappingTest(@RequestParam String param){
         log.debug("SampelGetMappingTest");
         log.debug("SampelGetMappingTest");
         log.debug("SampelGetMappingTest");
 
         // 안녕
         // 하세요
+        // Test
 
         return "success";
     }
